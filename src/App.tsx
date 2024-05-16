95565ce3ecc0163b47f4c9db76a9b786b57334e7
kazvee <kazveedev@gmail.com>
2024-05-16 15:18:30 -0600

    add dynamic image import with top level await

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import './App.css';
+const logoName = 'rubberduck'
+const module = await import(`./png-images/${logoName}.png`) as { default: string }; // top level await
 
 function App() {
   return (
     <div className='App'>
+      <div>
+        <img src={module.default} alt='logo' />
+      </div>
       <h1>Très Vite ⚡</h1>
       <a href='/another-route/'>Another Route</a> 🛣️
     </div>
