3b092a9091966a76079247e21471ec4c751f5e80
kazvee <kazveedev@gmail.com>
2024-05-16 14:50:58 -0600

    create another route

---
+++ src/another-route.ts
@@ -0,0 +1,5 @@
+document.getElementById('root')!.innerHTML = `
+<h1>This is another route! 🛣️</h1>`;
+
+export { };
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        another: resolve(__dirname, 'another-route/index.html'),
+      },
+    },
+  },
+});
