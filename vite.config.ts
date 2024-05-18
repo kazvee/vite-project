7c6e87b8746dac33d581349a39f78708637e299b
kazvee <kazveedev@gmail.com>
2024-05-18 10:55:48 -0600

    add `postcss-nesting` to config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import postCssNesting from 'postcss-nesting';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [postCssNesting()],
+    },
+  },
   build: {
     target: 'esnext',
     rollupOptions: {
