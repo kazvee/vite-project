2e58645a393e9381890bcb439478b9a40dc6a5e2
kazvee <kazveedev@gmail.com>
2024-05-18 11:53:44 -0600

    move home link to `another-route.ts` & add styling

--- src/another-route.ts
+++ src/another-route.ts
@@ -1,5 +1,13 @@
+import './App.css';
+import './index.css';
+
 document.getElementById('root')!.innerHTML = `
-<h1>This is another route! 🛣️</h1>`;
+  <div class="App">
+    <h1>This is another route! 🛣️</h1>
+    <h2>
+    <a href="/">Home</a> 🏡
+  </h2>
+`;
 
 export { };
 
