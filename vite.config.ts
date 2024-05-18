ebc486fff77c08eb57445899483902fca051aa04
kazvee <kazveedev@gmail.com>
2024-05-18 11:43:48 -0600

    optimize build config with vendor chunk handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import postCssNesting from 'postcss-nesting';
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   css: {
     postcss: {
       plugins: [postCssNesting()],
